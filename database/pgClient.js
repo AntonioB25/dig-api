621bbe7af987c86b429c9c8f97f98256e51e23b3
Antonio Bukovac <antoniobukovac10@gmail.com>
2023-01-17 10:08:41 +0100

    Added database connection string

--- database/pgClient.js
+++ database/pgClient.js
@@ -1,12 +1,18 @@
 const { Client } = require('pg')
- 
+
+const connectionString = "postgres://zcyjtpnt:v9WrgFEIVCryDxRnFn2s5xrZpXuqhEvG@mel.db.elephantsql.com/zcyjtpnt";
+
 const pgClient = new Client({
-  user: 'postgres', 
-  password: 'password', 
-  host: 'localhost', 
-  database: 'digobr', 
-  port: 5432, 
-})
+  connectionString: connectionString
+});
+
+// const pgClient = new Client({
+//   user: 'postgres', 
+//   password: 'password', 
+//   host: 'localhost', 
+//   database: 'digobr', 
+//   port: 5432, 
+// })
 
 pgClient.connect((err) => {
   if (err) {
